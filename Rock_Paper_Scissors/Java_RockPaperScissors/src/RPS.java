9a4ed96056f6a1de012024c75b8a441224ae44a4
Aashish11 <np.ashishsingh@gmail.com>
2021-02-16 14:22:35 -0500

    added encapsulation

--- Rock_Paper_Scissors/Java_RockPaperScissors/src/RPS.java
+++ Rock_Paper_Scissors/Java_RockPaperScissors/src/RPS.java
@@ -19,11 +19,45 @@ public class RPS
 		
 		// Default prompt message for the users.
 		String userMessage = "please select rock, paper or scissors";
+		
 		String [] gestures = {"rock", "paper", "scissors"};
 		
 		// Calls the scanner class
 		Scanner userInput = new Scanner(System.in);
 		
+		// Runs the program seven times and print outs the number of wins of each player,
+		// the number of draws, and the invalid gestures entered during the game.
+		gameOutcome(userInput, gestures, totalPlays, userMessage, invalidGames, p1Wins, p2Wins, draw);
+		
+	}
+	
+
+	
+	/**
+	 * Checks if the entries of the users matches to the stored gestures
+	 * of rock, paper, and scissors.
+	 */
+	public static boolean responseMatch(String p1, String[] gestures)
+	{
+		for (int i = 0; i < gestures.length; i++)
+		{
+			if (gestures[i].contentEquals(p1))
+			{
+				return true;
+			}
+		}
+		
+		return false;	
+	}
+	
+	
+	/**
+	 * Calculates the wins, draws, and invalid gestures entered during the game.
+	 */
+	private static void gameOutcome(Scanner userInput, String[] gestures, int totalPlays
+										   , String userMessage, int invalidGames, int p1Wins, int p2Wins
+										   , int draw)
+	{			
 		// Runs the program a total of 7 times and prints out the
 		// results at the end of the program.
 		for (int i = 0; i < totalPlays; i++)
@@ -94,27 +128,7 @@ public class RPS
 				 			  + "and there were " + draw + " ties. "
 				 			  + "There were a total of " + invalidGames
 				 			  + " invalid games.");
-
 		
-	}
-	
-
-	
-	/**
-	 * Checks if the entries of the users matches to the stored gestures
-	 * of rock, paper, and scissors.
-	 */
-	private static boolean responseMatch(String p1, String[] gestures)
-	{
-		for (int i = 0; i < gestures.length; i++)
-		{
-			if (gestures[i].contentEquals(p1))
-			{
-				return true;
-			}
-		}
-		
-		return false;	
 	}
 
 }
