c120dcef6cff60c5fbea46224b3181c94afd9203
Aashish11 <np.ashishsingh@gmail.com>
2021-12-11 13:07:16 -0500

    Updated comments

--- Coach_Effort_Allocation/Coach.py
+++ Coach_Effort_Allocation/Coach.py
@@ -17,24 +17,73 @@ managing: 75%
 '''
 from statistics import mean
 
+# --------------------------------------------------------------------------------------------------------- 
+# Creates a object of temporary schedule, which can be created for a coach multiple time or none at all.
+# --------------------------------------------------------------------------------------------------------- 
 class TemporarySchedule():
     def __init__(self, date: str, p_coaching: float, m_coaching: float, g_coaching: float) -> object:
+        '''
+        @param date
+        Date in which the temporary schedule overrides the coache's baseline effort allocation.
+        
+        @param p_coaching
+        Personal coaching allocation.
+        
+        @param m_coaching
+        Managing coaching allocation.
+        
+        @param g_coaching
+        Group coaching allocation.
+        '''
         self.temp_date = date
         self.t_p_coaching = p_coaching
         self.t_m_coaching = m_coaching
         self.t_g_coaching = g_coaching
         
-        
+
+# --------------------------------------------------------------------------------------------------------- 
+# Creates an object called coach, which houses functions that define the coache's activity.
+# ---------------------------------------------------------------------------------------------------------      
 class Coach():
     def __init__(self, name: str, p_coaching: float, m_coaching: float, g_coaching: float) -> object:
+        '''
+        @param name
+        Name of the coach.
+        
+        @param p_coaching
+        Personal coaching allocation.
+        
+        @param m_coaching
+        Managing coaching allocation.
+        
+        @param g_coaching
+        Group coaching allocation.
+        '''
         self.name = name
         self.temp_schedule = []
         self.b_p_coaching = p_coaching
         self.b_m_coaching = m_coaching
         self.b_g_coaching = g_coaching 
         
+    # A coach can have one or more temporary schedules and to account for that, the set_temp_schedule
+    # method creates one or more temporay schedule object.
+    def set_temp_schedule(self, date: str, p_coaching: float, m_coaching: float, g_coaching: float) -> list:
+        '''
+        @param date
+        Date in which the temporary schedule overrides the coache's baseline effort allocation.
         
-    def set_temp_schedule(self, date: str, p_coaching: float, m_coaching: float, g_coaching: float):
+        @param p_coaching
+        Personal coaching allocation.
+        
+        @param m_coaching
+        Managing coaching allocation.
+        
+        @param g_coaching
+        Group coaching allocation.
+        '''
+        
+        # Creates the temporary schedule object by passing the coach's temp schedule date,
+        # and the coaching allocations.
         schedule = TemporarySchedule(date, p_coaching, m_coaching, g_coaching)
         temp = []
         
@@ -44,27 +93,48 @@ class Coach():
         temp.append(schedule.t_g_coaching)
         
         self.temp_schedule.append(temp)
-                 
-    def calc_report(self, start_date, end_date):
         
+    # Calculates the coach's effort allcoation report based on the inputs.         
+    def calc_report(self, start_date, end_date):
+        '''
+        @param start_date
+        The start date to generate the effort allocation report for the coach.
+        
+        @param end_date
+        The end date to generate the effort allocation report for the coach.
+        '''
+        
+        # If there is no temporary schedule for a coach, the base line is 
+        # presented in the report.
         if len(self.temp_schedule) == 0:
             print('Report: ' + start_date + ' through ' + end_date)
             print('personal: ' + "{:.0%}".format(self.b_p_coaching))
             print('managing: ' + "{:.0%}".format(self.b_m_coaching))
+        
+        # If there is only one temporary schedule for a coach, the average
+        # of the baseline and the one temporary schedule is taken.
         elif len(self.temp_schedule) == 1:
             avg_p_coaching = mean([self.b_p_coaching, self.temp_schedule[0][1]])
             avg_m_coaching = mean([self.b_p_coaching, self.temp_schedule[0][2]])
             print('Report: ' + start_date + ' through ' + end_date)
             print('personal: ' + "{:.0%}".format(avg_p_coaching))
             print('managing: ' + "{:.0%}".format(avg_m_coaching))
+            
+        # If there are multiple temporary schedule for a coach, the average
+        # of the baseline and the multiple temporary schedule is taken.
         else:
+            # Empty lists to store the personal coaching allcoation and the
+            # managing coaching allocations.
             p_coaching_list = []
             m_coaching_list = []
             
+            # Iterates through the multiple temporary schedule.
             for index in range(len(self.temp_schedule)):
                 p_coaching_list.append(self.temp_schedule[index][1])
                 m_coaching_list.append(self.temp_schedule[index][2])
             
+            # Adds the baseline personal coaching and managing allocation
+            # which is used to create an average/mean effort percentage.
             p_coaching_list.append(self.b_p_coaching)
             m_coaching_list.append(self.b_m_coaching)
             
@@ -78,12 +148,20 @@ class Coach():
 
     
              
-        
+# Main executable program.      
 if __name__ == '__main__':
-    coach1 = Coach('Ashish', .50, .50, 0)   
+    
+    # Attributes of one coach and it's baseline.
+    coach1 = Coach('Ashish', .50, .50, 0)  
+     
+    # Assigns a temporary schedule to the same coach.
     coach1.set_temp_schedule('2021-10-15', 1, 0, 0)
+    
+    # For testing purposes, I passed multiple temporary schedules.
     #coach1.set_temp_schedule('2021-10-20', .25, .25, .5)
     #coach1.set_temp_schedule('2021-10-25', .50, .25, .25)
+    
+    # Generates the effort allocation report.
     coach1.calc_report('2021-10-14', '2021-10-15')
         
     
--- Coach_Effort_Allocation/coaching.py
+++ Coach_Effort_Allocation/coaching.py
@@ -1,4 +1,7 @@
 '''
+@author: Ashish Singh
+Created on: 2021-12-10
+
 baseline:
     personal: 50%
     managing: 50%
@@ -14,19 +17,52 @@ managing: 75%
 
 from statistics import mean
 
+# Variable to store the start and end date of the report.
 start_date = '2021-10-14'
 end_date = '2021-10-15'
 
+# Dictionary that stores the coach's employee ID and their baseline and temporary 
+# effort allocation.
+
+# Since the dictionary needs unique key, I'm passing the employee ID rather than name,
+# which I used on Friday. This way, we can store multiple coaches in the same dictionary
+# and if there are coaches with the same name, it won't have an issues with the data structure.
 base_coaching_alloc = {123456789: [.50, .50, 0]}
 temp_coaching_alloc = {123456789: [0, 1, 0]}
 
 
-def calc_report(start_date, end_date, emplID, base_allocation, temp_allocation):
+# Function takes on the report start date, end date, the coach's employee ID and their
+# base and temporary effort allocation.
+def calc_report(start_date, end_date, emplID, base_allocation, temp_allocation) -> list:
+    '''
+    @param start_date
+    The start date to generate the effort allocation report for the coach.
+        
+    @param end_date
+    The end date to generate the effort allocation report for the coach.
     
+    @param emplID
+    Employee ID of the coach.
+    
+    @param base_allocation
+    Base allocation of the coach.
+    
+    @param temp_allocation
+    Temporary schedule of the coach.
+    
+    @return list
+    A list that has the coach's personal, managing, and group effort allocation.
+    '''
+    
+    # If the date falls the temporary schedule date, it take the average of the 
+    # baseline and the temporary schedule.
     if start_date == '2021-10-15' or end_date == '2021-10-15':        
         avg_p_coach_alloc = mean([base_allocation[emplID][0], temp_allocation[emplID][0]])
         avg_m_coach_alloc = mean([base_allocation[emplID][1], temp_allocation[emplID][1]])
         avg_g_coach_alloc = mean([base_allocation[emplID][2], temp_allocation[emplID][2]])
+        
+    # If the date does not fall temporary schedule date, it then uses the coach's
+    # baseline effort allocation.
     else:
         avg_p_coach_alloc = base_allocation[emplID][0]
         avg_m_coach_alloc = base_allocation[emplID][1]
@@ -35,10 +71,16 @@ def calc_report(start_date, end_date, emplID, base_allocation, temp_allocation):
     return [avg_p_coach_alloc, avg_m_coach_alloc, avg_g_coach_alloc]
         
     
-
+# Main executable code.
 if __name__ == '__main__':
+    
+    # An empty list to store the results of the same coach or other different coaches.
     report = []
+    
+    # Calls the calc_report function to calculate the personal, managing and group effort allocation.
     results = calc_report(start_date, end_date, 123456789, base_coaching_alloc, temp_coaching_alloc)
+    
+    # Adds the results to the report list, which will be used to print.
     report.append(results)
     
     print("report: " + start_date + ' through ' + end_date)
