3f5744ea9a56884e8889844cebc8ca5fafa8321a
Aashish11 <np.ashishsingh@gmail.com>
2021-12-11 07:27:39 -0500

    Updated file

--- Coach_Effort_Allocation/coaching.py
+++ Coach_Effort_Allocation/coaching.py
@@ -17,20 +17,20 @@ from statistics import mean
 start_date = '2021-10-14'
 end_date = '2021-10-15'
 
-base_coaching_alloc = {'Ashish': [.50, .50, 0]}
-temp_coaching_alloc = {'Ashish': [0, 1, 0]}
+base_coaching_alloc = {123456789: [.50, .50, 0]}
+temp_coaching_alloc = {123456789: [0, 1, 0]}
 
 
-def calc_report(start_date, end_date, coach, base_allocation, temp_allocation):
+def calc_report(start_date, end_date, emplID, base_allocation, temp_allocation):
     
     if start_date == '2021-10-15' or end_date == '2021-10-15':        
-        avg_p_coach_alloc = mean([base_allocation[coach][0], temp_allocation[coach][0]])
-        avg_m_coach_alloc = mean([base_allocation[coach][1], temp_allocation[coach][1]])
-        avg_g_coach_alloc = mean([base_allocation[coach][2], temp_allocation[coach][2]])
+        avg_p_coach_alloc = mean([base_allocation[emplID][0], temp_allocation[emplID][0]])
+        avg_m_coach_alloc = mean([base_allocation[emplID][1], temp_allocation[emplID][1]])
+        avg_g_coach_alloc = mean([base_allocation[emplID][2], temp_allocation[emplID][2]])
     else:
-        avg_p_coach_alloc = base_allocation[coach][0]
-        avg_m_coach_alloc = base_allocation[coach][1]
-        avg_g_coach_alloc = base_allocation[coach][2]
+        avg_p_coach_alloc = base_allocation[emplID][0]
+        avg_m_coach_alloc = base_allocation[emplID][1]
+        avg_g_coach_alloc = base_allocation[emplID][2]
         
     return [avg_p_coach_alloc, avg_m_coach_alloc, avg_g_coach_alloc]
         
@@ -38,7 +38,7 @@ def calc_report(start_date, end_date, coach, base_allocation, temp_allocation):
 
 if __name__ == '__main__':
     report = []
-    results = calc_report(start_date, end_date, 'Ashish', base_coaching_alloc, temp_coaching_alloc)
+    results = calc_report(start_date, end_date, 123456789, base_coaching_alloc, temp_coaching_alloc)
     report.append(results)
     
     print("report: " + start_date + ' through ' + end_date)
