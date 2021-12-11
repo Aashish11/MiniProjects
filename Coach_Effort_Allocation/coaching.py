acf8be4fc69caa2cd4b52605ef04b0ef67c732d5
Aashish11 <np.ashishsingh@gmail.com>
2021-12-11 07:11:45 -0500

    'Updated file'

--- Coach_Effort_Allocation/coaching.py
+++ Coach_Effort_Allocation/coaching.py
@@ -17,33 +17,30 @@ from statistics import mean
 start_date = '2021-10-14'
 end_date = '2021-10-15'
 
-def get_coaching_percentage(name):
-    b_coaching_list = [.50, .50, 0]
-    t_coaching_list = [0, 1, 0]
-    
-    coaching_percentage = [b_coaching_list, t_coaching_list]
-    
-    return coaching_percentage
+base_coaching_alloc = {'Ashish': [.50, .50, 0]}
+temp_coaching_alloc = {'Ashish': [0, 1, 0]}
 
-def calc_report(start_date, end_date):
-    allocation = get_coaching_percentage('test name')
+
+def calc_report(start_date, end_date, coach, base_allocation, temp_allocation):
     
     if start_date == '2021-10-15' or end_date == '2021-10-15':        
-        avg_p_coach_alloc = mean([allocation[0][0], allocation[1][0]])
-        avg_m_coach_alloc = mean([allocation[0][1], allocation[1][1]])
-        avg_g_coach_alloc = mean([allocation[0][2], allocation[1][2]])
+        avg_p_coach_alloc = mean([base_allocation[coach][0], temp_allocation[coach][0]])
+        avg_m_coach_alloc = mean([base_allocation[coach][1], temp_allocation[coach][1]])
+        avg_g_coach_alloc = mean([base_allocation[coach][2], temp_allocation[coach][2]])
     else:
-        avg_p_coach_alloc = allocation[0][0]
-        avg_m_coach_alloc = allocation[0][1]
-        avg_g_coach_alloc = allocation[0][2]
+        avg_p_coach_alloc = base_allocation[coach][0]
+        avg_m_coach_alloc = base_allocation[coach][1]
+        avg_g_coach_alloc = base_allocation[coach][2]
         
     return [avg_p_coach_alloc, avg_m_coach_alloc, avg_g_coach_alloc]
         
     
 
 if __name__ == '__main__':
-    results = calc_report(start_date, end_date)
+    report = []
+    results = calc_report(start_date, end_date, 'Ashish', base_coaching_alloc, temp_coaching_alloc)
+    report.append(results)
     
     print("report: " + start_date + ' through ' + end_date)
-    print("personal: " + str(results[0] * 100) + '%')
-    print('managing: ' + str(results[1] * 100) + '%')
+    print('personal: ' + "{:.0%}".format(report[0][0]))
+    print('managing: ' + "{:.0%}".format(report[0][1]))
